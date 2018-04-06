89545fc1d2dd2c2ce8d61c7beb9a098a4fad6a1e
yukornienko <36741729+yukornienko@users.noreply.github.com>
2018-04-06 18:58:58 +0300

    Update HW3_python.py

--- HW3_python.py
+++ HW3_python.py
@@ -81,11 +81,11 @@ with open ("/Users/yukornienko/Downloads/test_kmer.fastq") as fastq:
     kmers_list = kmers.list_kmers()
     print(kmers_list)
     kmers.visualise([1, 150], [0, 100000])
-    
-# In[134]:
+    L = kmers.genome_length(20)
+    print(L)
 
-L = kmers.genome_length(20)
-print(L)
+
+
 
 # In[151]:
 
@@ -94,9 +94,6 @@ with open ("/Users/yukornienko/Downloads/test_kmer.fastq") as fastq:
     kmers_dict_2 = kmers_2.dict_all_kmers(fastq)
     kmers_list_2 = kmers_2.list_kmers()
     kmers_2.visualise([1, 400], [0, 150000])
-
-   
-# In[161]:
-L = kmers_2.genome_length(55)
-print(L)
+    L = kmers_2.genome_length(55)
+    print(L)
 
